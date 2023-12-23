64ab531263b93ae2c0898be562685c9a1daa96aa
Stuart Forrest <468146+slcp@users.noreply.github.com>
2023-12-23 15:13:35 +0000

    feat: integration test the replacement of resource on permissions update (#7)
    * feat: integration test the replacement of resource on permissions update

--- influxdbv2/resource_create_legacy_authorization_test.go
+++ influxdbv2/resource_create_legacy_authorization_test.go
@@ -11,6 +11,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
+var idOnCreate string
+
 func TestAccLegacyAuthorization(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -20,6 +22,11 @@ func TestAccLegacyAuthorization(t *testing.T) {
 			{
 				Config: testAccCreateLegacyAuthorization(),
 				Check: resource.ComposeTestCheckFunc(
+					func(s *terraform.State) error {
+						id := extractIdForResource(s, "influxdb-v2_legacy_authorization.acctest")
+						idOnCreate = id
+						return nil
+					},
 					resource.TestCheckResourceAttr("influxdb-v2_legacy_authorization.acctest", "org_id", os.Getenv("INFLUXDB_V2_ORG_ID")),
 					resource.TestCheckResourceAttr("influxdb-v2_legacy_authorization.acctest", "description", "Acceptance test legacy token"),
 					resource.TestCheckResourceAttr("influxdb-v2_legacy_authorization.acctest", "status", "inactive"),
@@ -43,6 +50,12 @@ func TestAccLegacyAuthorization(t *testing.T) {
 					resource.TestCheckResourceAttr("influxdb-v2_legacy_authorization.acctest", "org_id", os.Getenv("INFLUXDB_V2_ORG_ID")),
 					// permissions is a complex array... we'll just check it has the correct length
 					resource.TestCheckResourceAttr("influxdb-v2_legacy_authorization.acctest", "permissions.#", "2"),
+				),
+			},
+			{
+				Config: testAccLegacyUpdateAuthorizationPermissions(),
+				Check: resource.ComposeTestCheckFunc(
+					checkResourceHasBeenReplaced("influxdb-v2_legacy_authorization.acctest", &idOnCreate),
 				),
 			},
 		},
@@ -105,6 +118,26 @@ resource "influxdb-v2_legacy_authorization" "acctest" {
 `
 }
 
+func testAccLegacyUpdateAuthorizationPermissions() string {
+	return `
+resource "influxdb-v2_legacy_authorization" "acctest" {
+	org_id = "` + os.Getenv("INFLUXDB_V2_ORG_ID") + `"
+	description = "Acceptance test legacy token 2"
+	name = "a user name"
+	password = "another secure password"
+	status = "active"
+	permissions {
+		action = "read"
+		resource {
+			id = "` + os.Getenv("INFLUXDB_V2_BUCKET_ID") + `"
+			org_id = "` + os.Getenv("INFLUXDB_V2_ORG_ID") + `"
+			type = "buckets"
+		}
+	}
+}
+`
+}
+
 func testAccLegacyAuthorizationDestroyed(s *terraform.State) error {
 	addToken := func(ctx context.Context, req *http.Request) error {
 		req.Header.Add("Authorization", fmt.Sprintf("Token %s", os.Getenv("INFLUXDB_V2_TOKEN")))
@@ -121,4 +154,29 @@ func testAccLegacyAuthorizationDestroyed(s *terraform.State) error {
 	}
 
 	return nil
+}
+
+func extractIdForResource(s *terraform.State, name string) string {
+	is := findResourceInState(s, name)
+	v, _ := is.Attributes["id"]
+	return v
+}
+
+func findResourceInState(s *terraform.State, name string) *terraform.InstanceState {
+	ms := s.RootModule()
+	rs, ok := ms.Resources[name]
+	if !ok {
+		return nil
+	}
+	is := rs.Primary
+	return is
+}
+
+func checkResourceHasBeenReplaced(name string, oid *string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		if id := extractIdForResource(s, name); id == *oid {
+			return fmt.Errorf("idd should have changed but it is still %s", id)
+		}
+		return nil
+	}
 }
